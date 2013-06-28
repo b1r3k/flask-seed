389535911ce0ee630dd0b87454b7d393d4e3c6bf
b1r3k <lukasz dot jachym at gmail dot com>
2013-06-28 16:55:49 +0200

    removed distracting variables

--- flaskApp/config/settings.py
+++ flaskApp/config/settings.py
@@ -10,11 +10,5 @@
 ### all settings should be defined as uppercase!
 ### overriden by env vars (via forman & .env file)
 DEBUG = False
-DEBUG_TB_INTERCEPT_REDIRECTS = False
-TESTING = False
-DEBUG_TB_ENABLED = False
 SECRET_KEY = '\x1e\xfa\xbe1\xf2\xc49\xd6\xb4c\xf1\xb4\t\x9cb\xcf.Og{\x1e\n@\xf7'
 LOG_LEVEL = 'INFO'
-
-MONGODB_URI = 'mongodb://PUT_YOUR_DB_HERE'
-MONGODB_DBNAME = 'YOUR_DB_NAME'
