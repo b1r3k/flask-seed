0cc91a2addde781339a185196dd715c5f4554aa1
catty <ca77y@live.com>
2013-07-07 20:16:26 +0200

    App config makeover.
    Adds more flexibility by introducing config objects
    and inheritance structure between them.
    Adds *iml to .gitignore.

---
+++ flaskApp/config.py
@@ -0,0 +1,19 @@
+'''
+ * Author: ca77y
+ * Date: 7/07/13
+ * Time: 19:55 PM
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/.
+'''
+
+
+class Default(object):
+    DEBUG = False
+    SECRET_KEY = '\x1e\xfa\xbe1\xf2\xc49\xd6\xb4c\xf1\xb4\t\x9cb\xcf.Og{\x1e\n@\xf7'
+    LOG_LEVEL = 'WARNING'
+
+
+class Dev(Default):
+    DEBUG = True
+    LOG_LEVEL = 'INFO'
